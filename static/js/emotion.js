2f26290ccdd8d44bf468567315bbb3e225888616
Fahrurnaim <155805641+Fahrurnaim@users.noreply.github.com>
2025-08-16 22:18:42 +0700

    emotion.js

--- static/js/emotion.js
+++ static/js/emotion.js
@@ -1,7 +1,5 @@
-// Connect ke server Flask via WebSocket
 const socket = io("http://127.0.0.1:5000");
 
-// Terima data emosi dari server
 socket.on("emotion_update", (data) => {
   document.getElementById("emotion-box").innerText =
     `Emotion: ${data.emotion} | Confidence: ${Math.round(data.confidence * 100)}%`;
